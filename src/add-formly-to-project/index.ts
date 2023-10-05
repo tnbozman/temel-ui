9a23bed5615b53462bda5c69fe9de924f91ccbb2
Troy Boswell <troy.boswell@dewc.com>
2023-10-05 23:10:49 +1030

    feat: add formly building up

--- src/add-formly-to-project/index.ts
+++ src/add-formly-to-project/index.ts
@@ -1,10 +1,73 @@
-import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
-
+import {
+  Rule,
+  SchematicContext,
+  Tree,
+  chain,
+  apply,
+  url,
+  template,
+  move,
+  mergeWith,
+  externalSchematic,
+  MergeStrategy,
+  noop,
+} from '@angular-devkit/schematics';
+import { normalize } from 'path';
+import { setupOptions } from '../utils/options-util';
+import { addRelativeImportToFile /*insertChangeToTextFile*/ } from '../utils/ng-util';
 
 // You don't have to export the function as default. You can also have more than one rule factory
 // per file.
 export function addFormlyToProject(_options: any): Rule {
-  return (tree: Tree, _context: SchematicContext) => {
-    return tree;
+  return async (tree: Tree, _context: SchematicContext) => {
+    await setupOptions(tree, _options);
+    const { unitTest, project, path } = _options;
+    const projectSrcPath = normalize(path + '/');
+    const templateCopyPath = './files/src';
+    const templateMovePath = projectSrcPath;
+
+    const templateSource = apply(url(templateCopyPath), [template({ ..._options }), move(templateMovePath)]);
+
+    return chain([
+      unitTest
+        ? noop()
+        : externalSchematic('@ngx-formly/schematics', 'ng-add', {
+            uiTheme: 'material',
+            project: project,
+          }),
+
+      unitTest
+        ? noop()
+        : externalSchematic('@schematics/angular', 'component', {
+            name: 'pages/basic-form-page',
+            project: project,
+          }),
+
+      mergeWith(templateSource, MergeStrategy.Overwrite),
+      (tree: Tree) => {
+        const appModulePath = `${templateCopyPath}/app/app.module.ts`; // Update with your app module path
+        const appContent = tree.read(appModulePath);
+
+        if (!appContent) {
+          throw new Error(`Failed to read ${appModulePath}`);
+        }
+
+        const updatedAppContent = appContent
+          .toString()
+          .replace('ReactiveFormsModule', 'FormsModule, ReactiveFormsModule');
+
+        tree.overwrite(appModulePath, updatedAppContent);
+
+        const routesPath = `${templateCopyPath}/app/router/app.routes.ts`;
+        tree = addRelativeImportToFile(
+          tree,
+          routesPath,
+          'BasicFormPageComponent',
+          `${templateCopyPath}/app/pages/basic-form-page/basic-form-page.component`,
+        );
+
+        return tree;
+      },
+    ]);
   };
 }
--- src/add-formly-to-project/index_spec.ts
+++ src/add-formly-to-project/index_spec.ts
@@ -1,14 +1,52 @@
 import { Tree } from '@angular-devkit/schematics';
-import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
+import { SchematicTestRunner, UnitTestTree } from '@angular-devkit/schematics/testing';
 import * as path from 'path';
 
 const collectionPath = path.join(__dirname, '../collection.json');
 
 describe('add-formly-to-project', () => {
+  let mockTree: UnitTestTree;
   const appName = 'test-app';
+  const angularJsonPath = '/angular.json';
+  const projectPath = `/projects/${appName}`;
+
+  beforeEach(() => {
+    mockTree = new UnitTestTree(Tree.empty()); // start with an empty tree
+    mockTree.create(
+      '/projects/test-project/src/app/app.module.ts',
+      `import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }`,
+    ); // App module
+
+    // Add index.html to the tree with specified content
+    mockTree.create(
+      angularJsonPath,
+      JSON.stringify({
+        version: 1,
+        projects: {
+          'test-app': {
+            root: projectPath,
+            sourceRoot: `${projectPath}/src`,
+          },
+        },
+      }),
+    );
+  });
+
   it('works', async () => {
     const runner = new SchematicTestRunner('schematics', collectionPath);
-    const tree = await runner.runSchematic('add-formly-to-project', { project: appName }, Tree.empty());
+    const tree = await runner.runSchematic('add-formly-to-project', { project: appName, unitTest: true }, mockTree);
 
     expect(tree.files).toEqual([]);
   });
